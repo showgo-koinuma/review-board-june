12df8f541b3f016dcfd318982ca4c34f924a7997
showgo-koinuma <85728773+showgo-koinuma@users.noreply.github.com>
2023-06-02 23:49:30 +0900

    ダッシュ、vertical方向のmaxspeedの設定

--- Assets/1-1/Scripts/PlayerController.cs
+++ Assets/1-1/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float m_maxSpeed = 20f;
     [SerializeField] float m_jumpPower = 10f;
     [SerializeField] float m_grabSpeed = 3f;
+    [SerializeField] float m_dashSpeed = 100f;
+    [SerializeField] float m_dashTime = 0.1f;
 
     Rigidbody2D m_playerRb;
     Animator m_animator;
@@ -19,13 +22,15 @@ public class PlayerController : MonoBehaviour
     //ジャンプ
     bool m_isGround;
     //壁掴み
-    GameObject m_grabObject;
-    bool m_isPushGrab;
-    bool m_wallJump;
     float m_vertical;
     float gravity;
     float m_grabTimer;
     float m_grabAnim;
+    //ダッシュ
+    bool m_isCanDash;
+    Vector2 m_direction;
+    bool m_dashTimerIsStart;
+    float dashtimer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -42,15 +47,16 @@ public class PlayerController : MonoBehaviour
         m_horizontal = Input.GetAxisRaw("Horizontal");
         m_vertical = Input.GetAxisRaw("Vertical");
         //maxSpeed以上出ないように
-        if (m_playerRb.velocity.x > m_maxSpeed)
+        if (Math.Pow(m_playerRb.velocity.x, 2) > Math.Pow(m_maxSpeed, 2))
         {
+            float x = m_playerRb.velocity.x;
             float y = m_playerRb.velocity.y;
-            m_playerRb.velocity = new Vector2(m_maxSpeed, y);
+            m_playerRb.velocity = new Vector2(m_maxSpeed * Math.Sign(x), y);
         }
-        if (m_playerRb.velocity.x < m_maxSpeed * -1)
+        if (m_playerRb.velocity.y > m_jumpPower)
         {
-            float y = m_playerRb.velocity.y;
-            m_playerRb.velocity = new Vector2(m_maxSpeed * -1, y);
+            float x = m_playerRb.velocity.x;
+            m_playerRb.velocity = new Vector2(x, m_jumpPower);
         }
 
         //左右反転
@@ -72,7 +78,7 @@ public class PlayerController : MonoBehaviour
         //壁掴み
         //判定をlinecastする
         Vector2 start = this.transform.position;
-        Vector2 lineForGrab = new Vector2(transform.localScale.x, -0.6f);
+        Vector2 lineForGrab = new Vector2(transform.localScale.x * 0.9f, -0.7f);
         Debug.DrawLine(start, start + lineForGrab, Color.red);
         RaycastHit2D hit = Physics2D.Linecast(start, start + lineForGrab);
         if (Input.GetButton("Fire1") && hit.collider)
@@ -99,7 +105,11 @@ public class PlayerController : MonoBehaviour
                     m_lscaleX *= -1;
                     m_scale = new Vector2(m_lscaleX, transform.localScale.y);
                     transform.localScale = m_scale;
-                    m_wallJump = true;
+                    float x;
+                    if (transform.localScale.x > 0) { x = 1; }
+                    else { x = -1; }
+                    m_playerRb.AddForce(new Vector2(x, 1).normalized * m_jumpPower, ForceMode2D.Impulse);
+                    m_animator.Play("Jump");
                 }
                 m_animator.SetBool("Grab", true);
             }
@@ -111,23 +121,30 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            if (m_wallJump)
-            {
-                float x;
-                if (transform.localScale.x > 0) { x = 1; }
-                else { x = -1; }
-                m_playerRb.AddForce(new Vector2(x, 1).normalized * m_jumpPower, ForceMode2D.Impulse);
-                m_animator.Play("Jump");
-                m_wallJump = false;
-            }
             m_playerRb.gravityScale = gravity;
             m_animator.SetBool("Grab", false);
         }
 
         //ダッシュ
-        if (Input.GetButtonDown("Dash"))
+        if (Input.GetButtonDown("Dash") && m_isCanDash && m_vertical >= 0)
         {
-            Dash();
+            m_dashTimerIsStart = true;
+            m_direction = new Vector2(Math.Sign(transform.localScale.x), m_vertical).normalized;
+            if (m_horizontal == 0 && m_vertical > 0)
+            {
+                m_direction = Vector2.up;
+            }
+        }
+        if (m_dashTimerIsStart)
+        {
+            m_isCanDash = false;
+            m_playerRb.velocity = m_direction * m_dashSpeed;
+            dashtimer += Time.deltaTime;
+            if (dashtimer > m_dashTime)
+            {
+                dashtimer = 0f;
+                m_dashTimerIsStart = false;
+            }
         }
 
         //アニメーター
@@ -162,6 +179,7 @@ public class PlayerController : MonoBehaviour
         {
             m_isGround = true;
         }
+        //m_isGround = true;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -170,10 +188,11 @@ public class PlayerController : MonoBehaviour
         {
             m_isGround = false;
         }
+        //m_isGround = false;
     }
 
-    void Dash()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log("dash");
+        m_isCanDash = true;
     }
 }
